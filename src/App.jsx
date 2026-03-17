d0c75858272d4c0dc88aee04eb2eee4e1233ce0d
bharath kumar MS <bharath2005goo@gmail.com>
2026-03-17 09:06:42 +0530

    Initial Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-4xl font-bold text-blue-500'>
+      <h2>Welcome to Question Blank </h2>
+    </div>
+  )
+}
+
+export default App
